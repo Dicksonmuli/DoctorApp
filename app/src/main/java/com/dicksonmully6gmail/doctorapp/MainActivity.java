8bdb5008371d1663d63f7aeb09c9d84ae512d2fe
dicksonmuli <dicksonmully6@gmail.com>
2017-06-02 11:32:11 +0300

    add colors and fonts

--- app/src/main/java/com/dicksonmully6gmail/doctorapp/MainActivity.java
+++ app/src/main/java/com/dicksonmully6gmail/doctorapp/MainActivity.java
@@ -1,9 +1,17 @@
 package com.dicksonmully6gmail.doctorapp;
 
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -11,10 +19,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private SharedPreferences mSharedPreferences;
     private SharedPreferences.Editor mEditor;
 
+    // butterknife to make code DRY
+    @Bind(R.id.findConditionButton) Button mFindRestaurantsButton;
+    @Bind(R.id.conditionEditText) EditText mLocationEditText;
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+
+        Typeface alexBrushFont = Typeface.createFromAsset(getAssets(), "fonts/OpenSans-Regular.ttf");
+        mAppNameTextView.setTypeface(alexBrushFont);
+
+
+
+        mFindRestaurantsButton.setOnClickListener(this);
     }
 
     @Override
