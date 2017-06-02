da228ebc6b626d9cfb5f733253cf56679c9063ba
dicksonmuli <dicksonmully6@gmail.com>
2017-06-02 18:11:16 +0300

    t
    altering JSON arrays

---
+++ app/src/main/java/com/dicksonmully6gmail/doctorapp/Constants.java
@@ -0,0 +1,11 @@
+package com.dicksonmully6gmail.doctorapp;
+
+/**
+ * Created by dickson on 6/2/17.
+ */
+
+public class Constants {
+    public static final String BETTER_DOCTOR_API = BuildConfig.BETTER_DOCTOR_API;
+    public static final String API_BASE_URL = "https://api.betterdoctor.com/2016-03-01/doctors?location=";
+    public static final String BETTER_DOCTOR_APPID = "10&skip=0&limit=10&user_key=";
+}
--- app/src/main/java/com/dicksonmully6gmail/doctorapp/services/BetterDoctorService.java
+++ app/src/main/java/com/dicksonmully6gmail/doctorapp/services/BetterDoctorService.java
@@ -2,6 +2,9 @@ package com.dicksonmully6gmail.doctorapp.services;
 
 import android.provider.SyncStateContract;
 
+import com.dicksonmully6gmail.doctorapp.Constants;
+import com.dicksonmully6gmail.doctorapp.models.Doctor;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -27,53 +30,63 @@ import okhttp3.Response;
         public static void findRestaurants(String location, Callback callback) {
 //
 //        HttpUrl class to construct the URL we'll send our request to
-            HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.YELP_BASE_URL).newBuilder();
-            urlBuilder.addQueryParameter(SyncStateContract.Constants.YELP_LOCATION_QUERY_PARAMETER, location);
-            String url = urlBuilder.build().toString();
+            HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.API_BASE_URL).newBuilder();
+            String url = urlBuilder.build().toString() + location + Constants.BETTER_DOCTOR_APPID + "key=" + Constants.BETTER_DOCTOR_API;
 
 //       create request using the created url
             Request request= new Request.Builder()
-                    .header("Authorization", "Bearer " + Constants.YELP_TOKEN)
                     .url(url)
                     .build();
 //        execute this request
             Call call = client.newCall(request);
             call.enqueue(callback);
         }
-        //        parse JSON data and return a list of restaurants
-        public ArrayList<Restaurant> processResults(Response response) {
-            ArrayList<Restaurant> restaurants = new ArrayList<>();
+        //        parse JSON data and return a list of doctors
+        public ArrayList<Doctor> processResults(Response response) {
+            ArrayList<Doctor> restaurants = new ArrayList<>();
 
             try {
                 String jsonData = response.body().string();
                 if (response.isSuccessful()) {
-                    JSONObject yelpJSON = new JSONObject(jsonData);
-                    JSONArray businessesJSON = yelpJSON.getJSONArray("businesses");
-                    for (int i = 0; i < businessesJSON.length(); i++) {
-                        JSONObject restaurantJSON = businessesJSON.getJSONObject(i);
-                        String name = restaurantJSON.getString("name");
+                    JSONObject betterDoctorJSON = new JSONObject(jsonData);
+                    JSONArray dataJSON = betterDoctorJSON.getJSONArray("data");
+                    for (int i = 0; i < dataJSON.length(); i++) {
+                        JSONObject doctorJSON = dataJSON.getJSONObject(i);
+
+                        String first_name = doctorJSON.getJSONObject("profile").getString("first_name");
+                        String last_name = doctorJSON.getJSONObject("profile").getString("last_name");
+                        String imageUrl = doctorJSON.getJSONObject("profile").getString("image_url");
+                        String gender = doctorJSON.getJSONObject("profile").getString("gender");
+                        String bio = doctorJSON.getJSONObject("profile").getString("bio");
+
                         String phone = restaurantJSON.optString("display_phone", "Phone not available");
-                        String website = restaurantJSON.getString("url");
-                        double rating = restaurantJSON.getDouble("rating");
-                        String imageUrl = restaurantJSON.getString("image_url");
+                        //website
+                        JSONArray practicesJSON = doctorJSON.getJSONArray("practices");
+                        String website = practicesJSON.getJSONObject(1).getString("website");
+                        //lon lat
+                        String lat = practicesJSON.getJSONObject(1).getString("lat");
+                        String lon = practicesJSON.getJSONObject(1).getString("lon");
+
+                        double rating = restaurantJSON.getDouble("ratings");
                         double latitude = restaurantJSON
                                 .getJSONObject("coordinates").getDouble("latitude");
                         double longitude = restaurantJSON
                                 .getJSONObject("coordinates").getDouble("longitude");
-                        ArrayList<String> address = new ArrayList<>();
-                        JSONArray addressJSON = restaurantJSON.getJSONObject("location")
-                                .getJSONArray("display_address");
-                        for (int y = 0; y < addressJSON.length(); y++) {
-                            address.add(addressJSON.toString());
+
+                        ArrayList<String> specialties = new ArrayList<>();
+                        JSONArray specialtyJSON = doctorJSON.getJSONArray("specialties")
+                                .getJSONArray(2);
+                        for (int y = 0; y < specialtyJSON.length(); y++) {
+                            specialties.add(specialtyJSON.toString());
                         }
 
-                        ArrayList<String> categories = new ArrayList<>();
+                        ArrayList<String> specialities = new ArrayList<>();
                         JSONArray categoriesJSON = restaurantJSON.getJSONArray("categories");
 
                         for (int y = 0; y < categoriesJSON.length(); y++) {
                             categories.add(categoriesJSON.getJSONObject(y).getString("title").toString());
                         }
-                        Restaurant restaurant = new Restaurant(name, phone, website, rating,
+                        Doctor restaurant = new Doctor(name, phone, website, rating,
                                 imageUrl, latitude, longitude, address, categories);
                         restaurants.add(restaurant);
                     }
