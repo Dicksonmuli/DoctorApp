927db4e494e64ff64a2c6e3f4aaf40d17544fed4
dicksonmuli <dicksonmully6@gmail.com>
2017-06-08 14:55:00 +0300

    overriding onstart and onstop events to add and remove user when they login and logout respectively

--- app/src/main/java/com/dicksonmully6gmail/doctorapp/ui/MainActivity.java
+++ app/src/main/java/com/dicksonmully6gmail/doctorapp/ui/MainActivity.java
@@ -65,6 +65,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         };
     }
+    //overriding onstart
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    //overriding onStop
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
 
     @Override
     public void onClick(View v) {
