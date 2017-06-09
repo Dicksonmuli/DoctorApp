4d9e9bf81b1430f3a4c05dadd15a5c1315eafdd6
dicksonmuli <dicksonmully6@gmail.com>
2017-06-09 15:05:19 +0300

    authenticating progress bar on login

--- app/src/main/java/com/dicksonmully6gmail/doctorapp/ui/LoginActivity.java
+++ app/src/main/java/com/dicksonmully6gmail/doctorapp/ui/LoginActivity.java
@@ -1,14 +1,18 @@
 package com.dicksonmully6gmail.doctorapp.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.dicksonmully6gmail.doctorapp.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -19,7 +23,7 @@ import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
-    @Bind(R.id.passwordLoginButton) Bundle mPasswordLoginButton;
+    @Bind(R.id.passwordLoginButton) Button mPasswordLoginButton;
     @Bind(R.id.emailEditText) EditText mEmailEditText;
     @Bind(R.id.passwordEditText) EditText mPasswordEditText;
     @Bind(R.id.registerTextView) TextView mRegisterTextView;
@@ -38,10 +42,53 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mAuth = FirebaseAuth.getInstance();
         mRegisterTextView.setOnClickListener(this);
         createAuthProgressDialog();
+
+//        click listener for mPasswordEditText
+        mPasswordLoginButton.setOnClickListener(this);
+        //authStateListener to check if the user is loged in
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
     }
     @Override
-    public void onClick(View v) {
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
 
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterTextView) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        if (view == mPasswordLoginButton) {
+            loginWithPassword();
+        }
+    }
+    //authenticating progress bar on login
+    private void createAuthProgressDialog() {
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle("Loading...");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mAuthProgressDialog.setCancelable(false);
     }
 
 
