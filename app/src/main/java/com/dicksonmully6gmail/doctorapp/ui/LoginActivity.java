e5819ad77e03074595c09331b06508637282d14b
dicksonmuli <dicksonmully6@gmail.com>
2017-06-09 15:07:54 +0300

    Firebase and authenticating the account with the user-provided credentials

--- app/src/main/java/com/dicksonmully6gmail/doctorapp/ui/LoginActivity.java
+++ app/src/main/java/com/dicksonmully6gmail/doctorapp/ui/LoginActivity.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import android.app.ProgressDialog;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.dicksonmully6gmail.doctorapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -89,6 +94,39 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mAuthProgressDialog.setTitle("Loading...");
         mAuthProgressDialog.setMessage("Authenticating with Firebase...");
         mAuthProgressDialog.setCancelable(false);
+    }
+//    Firebase and authenticating the account with the user-provided credentials
+    private void loginWithPassword() {
+        String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        if (email.equals("")) {
+            mEmailEditText.setError("Please enter your email");
+            return;
+        }
+
+        if (password.equals("")) {
+            mPasswordEditText.setError("Password cannot be blank");
+            return;
+        }
+        /**
+         *  adding onComplete listener
+         *  -is responsible for determining when the authentication attempt is complete,
+         *  and executes the onComplete() override when it is
+         */
+        mAuthProgressDialog.show();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener< AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG,"singInWithEmail:onComplete:" + task.isSuccessful());
+                        mAuthProgressDialog.dismiss();
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    }
+                });
     }
 
 
