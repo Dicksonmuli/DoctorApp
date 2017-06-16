ccfd2bf593c855d99f1ced238630a55884ec6166
dicksonmuli <dicksonmully6@gmail.com>
2017-06-16 11:40:54 +0300

    layout representing a single item of doctor drag list

---
+++ app/src/main/java/com/dicksonmully6gmail/doctorapp/util/OnStartDragListener.java
@@ -0,0 +1,11 @@
+package com.dicksonmully6gmail.doctorapp.util;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by dickson on 6/16/17.
+ */
+
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
